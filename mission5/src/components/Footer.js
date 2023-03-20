854a6fb86815a5ee09805e7cd2c0cc6b61ef3902
CyraMoss <cyramoss@icloud.com>
2023-03-20 22:27:34 +1300

    footer done

--- mission5/src/components/Footer.js
+++ mission5/src/components/Footer.js
@@ -6,13 +6,17 @@ export default function Footer() {
       <div className="footerlists">
         <div className="section">
           <p className="orange">Shop By Department</p>
-          <span className="body2">Computers & Laptops</span>
-          <span className="body2">PC Peripherals</span>
-          <span className="body2">PC Parts</span>
-          <span className="body2">Software</span>
-          <span className="body2">Gaming</span>
-          <span className="body2">Phones & Smart Devices</span>
-          <span className="body2">Headphones & Audio</span>
+          <div className="row ">
+            <span className="body2">Computers & Laptops</span>
+            <span className="body2">PC Peripherals</span>
+            <span className="body2">PC Parts</span>
+            <span className="body2">Software</span>
+            <span className="body2">Gaming</span>
+            <span className="body2">Phones & Smart Devices</span>
+            <span className="body2">Headphones & Audio</span>
+          </div>
+        </div>
+        <div className="row down">
           <span className="body2">TV & AV</span>
           <span className="body2">Cameras & Drones</span>
           <span className="body2">Networking</span>
@@ -22,45 +26,51 @@ export default function Footer() {
           <span className="body2">Apple</span>
           <span className="body2">More</span>
         </div>
-        ;
+
         <div className="section">
           <p className="orange">Company Info</p>
-          <span className="body2">About Us</span>
-          <span className="body2">Business</span>
-          <span className="body2">Education</span>
-          <span className="body2">Wholesale</span>
-          <span className="body2">Government</span>
-          <span className="body2">Health</span>
-          <span className="body2">Stores</span>
-          <span className="body2">Careers</span>
-          <span className="body2">Contact Us</span>
-          <span className="body2">News</span>
-          <span className="body2">Terms & Conditions</span>
-          <span className="body2">Privacy Policy</span>
+          <div className="row">
+            <span className="body2">About Us</span>
+            <span className="body2">Business</span>
+            <span className="body2">Education</span>
+            <span className="body2">Wholesale</span>
+            <span className="body2">Government</span>
+            <span className="body2">Health</span>
+            <span className="body2">Stores</span>
+            <span className="body2">Careers</span>
+            <span className="body2">Contact Us</span>
+            <span className="body2">News</span>
+            <span className="body2">Terms & Conditions</span>
+            <span className="body2">Privacy Policy</span>
+          </div>
         </div>
         <div className="section">
           <p className="orange">Services & Support</p>
-          <span className="body2">Help </span>
-          <span className="body2">Returns & Warranty</span>
-          <span className="body2">Finance</span>
-          <span className="body2">IT Services</span>
-          <span className="body2">Home Services</span>
-          <span className="body2">Repair Services</span>
-          <span className="body2">Job Tracker</span>
-          <span className="body2">Service Parts</span>
-          <span className="body2">SystemBuilder</span>
+          <div className="row">
+            <span className="body2">Help </span>
+            <span className="body2">Returns & Warranty</span>
+            <span className="body2">Finance</span>
+            <span className="body2">IT Services</span>
+            <span className="body2">Home Services</span>
+            <span className="body2">Repair Services</span>
+            <span className="body2">Job Tracker</span>
+            <span className="body2">Service Parts</span>
+            <span className="body2">SystemBuilder</span>
+          </div>
         </div>
         <div className="section">
           <p className="orange">Promotions & Offers</p>
-          <span className="body2">Promotions</span>
-          <span className="body2">Clearance</span>
-          <span className="body2">Hot Deals</span>
-          <span className="body2">New Arrivals</span>
-          <span className="body2">BYOD</span>
-          <span className="body2">Tax Free Shopping</span>
-          <span className="body2">Events</span>
-          <span className="body2">PB Insider</span>
-          <span className="body2">PB Advisor</span>
+          <div className="row">
+            <span className="body2">Promotions</span>
+            <span className="body2">Clearance</span>
+            <span className="body2">Hot Deals</span>
+            <span className="body2">New Arrivals</span>
+            <span className="body2">BYOD</span>
+            <span className="body2">Tax Free Shopping</span>
+            <span className="body2">Events</span>
+            <span className="body2">PB Insider</span>
+            <span className="body2">PB Advisor</span>
+          </div>
         </div>
       </div>
       <div className="footericons">
